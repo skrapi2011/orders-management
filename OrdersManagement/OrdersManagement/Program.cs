4027a101329877e1ba70f4bbeb4eac0572a02731
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-13 16:37:35 +0100

    Initial commit

---
+++ OrdersManagement/OrdersManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
