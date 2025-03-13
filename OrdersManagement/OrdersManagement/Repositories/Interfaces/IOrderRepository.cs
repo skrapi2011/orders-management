09c4e9c70661dfd5bb2a0223e5a544d4ccab356e
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-13 20:20:05 +0100

    Added OrderRepository

---
+++ OrdersManagement/OrdersManagement/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using OrdersManagement.Models;
+
+namespace OrdersManagement.Repositories.Interfaces;
+
+/// <summary>
+/// Represents an order repository.
+/// </summary>
+public interface IOrderRepository
+{
+    
+    /// <summary>
+    /// Gets all orders.
+    /// </summary>
+    /// <returns>List of orders</returns>
+    public List<Order> GetOrders();
+    
+    /// <summary>
+    /// Creates an order.
+    /// </summary>
+    /// <param name="order">Order object to be created</param>
+    /// <returns>Order object</returns>
+    public Order CreateOrder(Order order);
+    
+}
---
+++ OrdersManagement/OrdersManagement/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using OrdersManagement.Models;
+using OrdersManagement.Repositories.Interfaces;
+
+namespace OrdersManagement.Repositories;
+
+/// <summary>
+/// Represents an order repository.
+/// </summary>
+public class OrderRepository : IOrderRepository
+{
+    /// <summary>
+    /// List of orders, imitating a database.
+    /// </summary>
+    private List<Order> _orders = [];
+    
+    /// <summary>
+    /// Gets all orders.
+    /// </summary>
+    /// <returns>List of orders</returns>
+    public List<Order> GetOrders()
+    {
+        return _orders;
+    }
+    
+    /// <summary>
+    /// Creates an order.
+    /// </summary>
+    /// <param name="order">Order object to be created</param>
+    /// <returns>Order object</returns>
+    public Order CreateOrder(Order order)
+    {
+        _orders.Add(order);
+        return order;
+    }
+
+}
