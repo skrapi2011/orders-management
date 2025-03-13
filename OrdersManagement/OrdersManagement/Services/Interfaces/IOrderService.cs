98b5a2c40058726704463c572a59b94dc0e629ff
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-13 19:19:41 +0100

    Created OrderService

---
+++ OrdersManagement/OrdersManagement/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,26 @@
+﻿using OrdersManagement.Models;
+using OrdersManagement.Models.Dtos;
+
+namespace OrdersManagement.Services.Interfaces;
+
+/// <summary>
+/// Represents an order service.
+/// </summary>
+public interface IOrderService
+{
+
+    /// <summary>
+    /// Gets all orders.
+    /// </summary>
+    /// <returns>List of order response objects</returns>
+    public List<OrderResponseDto> GetOrders();
+    
+    /// <summary>
+    /// Creates an order.
+    /// </summary>
+    /// <param name="order">Order object to be created</param>
+    /// <returns>Order response object</returns>
+    public OrderResponseDto CreateOrder(OrderCreateDto order);
+    
+    
+}
---
+++ OrdersManagement/OrdersManagement/Services/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using OrdersManagement.Models.Dtos;
+using OrdersManagement.Services.Interfaces;
+
+namespace OrdersManagement.Services;
+
+/// <summary>
+/// Represents an order service.
+/// </summary>
+public class OrderService : IOrderService
+{
+    /// <summary>
+    /// Gets all orders.
+    /// </summary>
+    /// <returns>List of order response objects</returns>
+    public List<OrderResponseDto> GetOrders()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Creates an order.
+    /// </summary>
+    /// <param name="order">Order object to be created</param>
+    /// <returns>Order response object</returns>
+    public OrderResponseDto CreateOrder(OrderCreateDto order)
+    {
+        throw new NotImplementedException();
+    }
+}
