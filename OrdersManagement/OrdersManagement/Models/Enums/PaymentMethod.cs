e3cec46b2e411f0cef4ff1269d2212f03ea7c3b9
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-13 18:47:49 +0100

    Added enums

---
+++ OrdersManagement/OrdersManagement/Models/Enums/PaymentMethod.cs
@@ -0,0 +1,17 @@
+﻿namespace OrdersManagement.Models.Enums;
+
+/// <summary>
+/// Represents a payment method.
+/// </summary>
+public enum PaymentMethod
+{
+    /// <summary>
+    /// Payment by card.
+    /// </summary>
+    Card = 1,
+    
+    /// <summary>
+    /// Cash on delivery.
+    /// </summary>
+    CashOnDelivery = 2
+}
