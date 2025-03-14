76b01f69ed94cd7e0548acc8665b2f1868965e7e
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-14 16:59:06 +0100

    Create helper Mapping class

---
+++ OrdersManagement/OrdersManagement/Utils/Mapping.cs
@@ -0,0 +1,30 @@
+﻿using OrdersManagement.Models;
+using OrdersManagement.Models.Dtos;
+
+namespace OrdersManagement.Utils;
+
+/// <summary>
+/// Contains mapping methods.
+/// </summary>
+public abstract class Mapping
+{
+    /// <summary>
+    /// Maps an Order to OrderResponseDto.
+    /// </summary>
+    /// <param name="order">Order object</param>
+    /// <returns>OrderResponseDto object</returns>
+    public static OrderResponseDto MapToOrderResponseDto(Order order)
+    {
+        return new OrderResponseDto
+        {
+            Id = order.Id,
+            Amount = order.Amount,
+            ProductName = order.ProductName,
+            CustomerType = order.CustomerType,
+            DeliveryAddress = order.DeliveryAddress,
+            PaymentMethod = order.PaymentMethod,
+            OrderStatus = order.OrderStatus,
+            CreatedAt = order.CreatedAt
+        };
+    }
+}
