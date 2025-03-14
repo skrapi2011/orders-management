94e980861746475e5e3e9ea6b28fff43b5a994ca
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-03-14 17:59:08 +0100

    Invoke menuManager in Program.cs

--- OrdersManagement/OrdersManagement/Program.cs
+++ OrdersManagement/OrdersManagement/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OrdersManagement.data;
-using OrdersManagement.Models.Dtos;
 using OrdersManagement.Repositories;
 using OrdersManagement.Repositories.Interfaces;
 using OrdersManagement.Services;
 using OrdersManagement.Services.Interfaces;
+using OrdersManagement.UI;
 
 namespace OrdersManagement
 {
@@ -14,49 +14,18 @@ namespace OrdersManagement
         static async Task Main(string[] args)
         {
             var serviceProvider = ConfigureServices();
-    
+
             using (var scope = serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<OrdersDbContext>();
                 await dbContext.Database.EnsureCreatedAsync();
             }
-    
+
             using (var scope = serviceProvider.CreateScope())
             {
                 var orderService = scope.ServiceProvider.GetRequiredService<IOrderService>();
-        
-                var orderDto = new OrderCreateDto
-                {
-                    Amount = 100,
-                    ProductName = "Test Product",
-                    CustomerType = Models.Enums.CustomerType.Person,
-                    DeliveryAddress = "Address",
-                    PaymentMethod = Models.Enums.PaymentMethod.Card
-                };
-        
-                var createResult = await orderService.CreateOrderAsync(orderDto);
-                Console.WriteLine($"Order created: {!createResult.IsError}");
-
-                if (createResult.Value != null)
-                {
-                    var moveResult = await orderService.MoveToWarehouseAsync(createResult.Value.Id);
-                    if (moveResult.Value != null)
-                        Console.WriteLine(
-                            $"Order moved to warehouse: {!moveResult.IsError}, {moveResult.Value.OrderStatus}");
-                    
-                    var shipResult = await orderService.ShipOrderAsync(createResult.Value.Id);
-                    if (shipResult.Value != null)
-                        Console.WriteLine(
-                            $"Order shipped: {!shipResult.IsError}, {shipResult.Value.OrderStatus}, {shipResult.Value.Id}");
-
-                    if (shipResult.Value != null)
-                    {
-                        Console.WriteLine("Waiting for order to be shipped...");
-                        await Task.Delay(6000);
-                        var getResults = await orderService.GetOrderByIdAsync(shipResult.Value.Id);
-                        if (getResults.Value != null) Console.Write("New Order status "+getResults.Value.OrderStatus);
-                    }
-                }
+                var menuManager = new MenuManager(orderService);
+                await menuManager.RunAsync();
             }
         }
 
